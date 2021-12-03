5f3c93292abbd1917f278b1ebda5879b639a0623
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-02 23:15:26 -0300

    feat: create component title

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Title: React.FC = () => (
+  <Container>
+    <h1>Cabelex</h1>
+    <span>Venda de produtos de beleza</span>
+  </Container>
+);
+export default Title;
