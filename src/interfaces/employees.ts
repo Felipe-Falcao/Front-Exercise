5ddc81c36a765e2e527ba553500d886eb56d4101
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-02 23:21:08 -0300

    refactor: global interfaces

---
+++ src/interfaces/employees.ts
@@ -0,0 +1,11 @@
+export interface ListEmployeesDTO {
+  id: string;
+  name: string;
+  branch_name: string;
+  branch_id: string;
+}
+
+export interface EmployeeFormData {
+  name: string;
+  branch_name: string;
+}
---
+++ src/interfaces/params.ts
@@ -0,0 +1,8 @@
+export interface ParamTypes {
+  id: string;
+}
+
+export interface ParamDoubleIdTypes {
+  branch_id: string;
+  id: string;
+}
