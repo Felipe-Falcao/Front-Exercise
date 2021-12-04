48b6870390a84288cb05a6e8293869880ccef3e2
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-03 22:06:22 -0300

    feat: buttonHeader component

---
+++ src/components/ButtonHeader/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { shade } from 'polished';
+
+export const Container = styled.button`
+  background: #171717;
+  height: 30px;
+  border-radius: 5px;
+  border: 0;
+  padding: 5px 16px;
+  color: #fff;
+  width: 100%;
+  margin-top: 16px;
+  transition: background-color 0.2s;
+  &:hover {
+    background: ${shade(0.2, '#6F0102')};
+  }
+`;
