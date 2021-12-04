fca20762d5c136a5e534a962920cf25f318d208a
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-04 11:55:09 -0300

    style: create logo

--- src/components/Title/index.tsx
+++ src/components/Title/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-
-import { Container } from './styles';
+import Logo from '../../assets/images/logo.svg'
+import { Container, Items } from './styles';
 
 const Title: React.FC = () => (
   <Container>
-    <h1>Cabelex</h1>
-    <span>Venda de produtos de beleza</span>
+    <Items>
+      <h1>Cabelex</h1>
+      <img src={Logo} alt="Logo Cabelex" />
+      <span>Venda de produtos de beleza</span>
+    </Items>
   </Container>
 );
 export default Title;
--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -2,15 +2,33 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
   margin: auto auto auto 0;
+`;
+
+export const Items = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   h1 {
     font-size: 50px;
     font-family: 'Courier New', Courier, monospace;
     font-weight: 800;
+    color: #9400D3;
+  }
+
+  img {
+    width: 5rem;
+    padding: 0.7rem;
   }
 
   span {
     font-weight: 500;
+    color: #9400D3;
   }
 `;
