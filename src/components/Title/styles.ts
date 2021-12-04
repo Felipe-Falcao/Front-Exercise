c6725b6572888500ac33114df6c8d45f8503da05
Felipe-Falcao <felipe.falcao@dcomp.ufs.br>
2021-12-04 11:35:52 -0300

    style: update content

--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -2,12 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-
-  margin-top: 20px;
+  margin: auto auto auto 0;
 
   h1 {
     font-size: 50px;
